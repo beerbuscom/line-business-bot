5bd47246bd96a546dea65717d887ff482bd2b4ab
beerbuscom <beerbuscom86@gmail.com>
2017-09-06 12:02:30 +0700

    Update index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (!is_null($events['events'])) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
 			// Get text sent
-			$text = $event['message']['text']. "\r\n".$event['user']['userId'];
+			$text = $event['message']['text']. "\r\n".json_encode($event);
 			// Get replyToken
 			$replyToken = $event['replyToken'];
 
