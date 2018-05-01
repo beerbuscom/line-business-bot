ebfcc0daba55d173f54a72cebc7565655795aaa1
beerbuscom <beerbuscom86@gmail.com>
2018-05-01 16:22:29 +0700

    Update index.php

--- index.php
+++ index.php
@@ -7,7 +7,7 @@ $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
 // Validate parsed JSON data
-//if (!is_null($events['events'])) {
+if (!is_null($events['events'])) {
 	// Loop through each event
 	foreach ($events['events'] as $event) {
 		// Reply only when message sent is in 'text' format
@@ -47,7 +47,7 @@ $events = json_decode($content, true);
 			}
 		}
 	}
-//}
+}
 
 ?>
 
