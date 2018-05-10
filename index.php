11a50b3c9c43281d8f6258236f787c3d22a553d9
beerbuscom <beerbuscom86@gmail.com>
2018-05-10 10:22:13 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,55 +1,316 @@
 <?php
 
-$access_token = 'eF1ERIEGeVZTqewnN/40ZB29z+ymgX5as0il48qpuviVbhSQkETK7SbZRf3ugvuLFbyNDQbEnUQ4Ug2ePTiTr13ohmw5faJJmZTCSrA4Rru1wRN3O7Yz6Lp8DRDCS90MhlHPwDDBldQe/auHMfjODgdB04t89/1O/w1cDnyilFU=';
+
+
+function getProfile($access_token,$userID){
+    
+
+	$arrHeader = array();
+	$arrHeader[] = "Content-Type: application/json";
+	$arrHeader[] = "Authorization: Bearer {$access_token}";
+	
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,'https://api.line.me/v2/bot/profile/'.$userID);
+	//curl_setopt($ch, CURLOPT_HEADER, false);
+	//curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+	//curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	
+	
+	
+	$result = curl_exec($ch);
+	//echo curl_error($ch);	
+	curl_close ($ch);
+	
+	return ($result);
+}
+
+function pushMessage_text($access_token,$userID,$text){
+    
+
+	$arrHeader = array();
+	$arrHeader[] = "Content-Type: application/json";
+	$arrHeader[] = "Authorization: Bearer {$access_token}";
+	
+	$arrPostData = array();
+	$arrPostData['to'] = $userID;
+	$arrPostData['messages'][0]['type'] = "text";
+	$arrPostData['messages'][0]['text'] = $text;
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,'https://api.line.me/v2/bot/message/push');
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	
+	
+	
+	$result = curl_exec($ch);
+	//echo curl_error($ch);	
+	curl_close ($ch);
+	
+	return ($result);
+}
+
+function pushMessage_sticker($access_token,$userID,$packageId,$stickerId){
+    
+
+	$arrHeader = array();
+	$arrHeader[] = "Content-Type: application/json";
+	$arrHeader[] = "Authorization: Bearer {$access_token}";
+	
+	$arrPostData = array();
+	$arrPostData['to'] = $userID;
+	$arrPostData['messages'][0]['type'] = "sticker";
+	$arrPostData['messages'][0]['packageId'] = $packageId;
+	$arrPostData['messages'][0]['stickerId'] = $stickerId;
+	
+	{
+  
+}
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,'https://api.line.me/v2/bot/message/push');
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	
+	
+	
+	$result = curl_exec($ch);
+	//echo curl_error($ch);	
+	curl_close ($ch);
+	
+	return ($result);
+}
+function pushMessage_template_image_carousel($access_token,$userID,$arrayData){
+	
+	/*$json = '{
+				  "type": "template",
+				 
+				  "template": {
+				      "type": "image_carousel",
+				      "columns": [
+				          {
+				            "imageUrl": "http://www.catster.com/wp-content/uploads/2017/08/Pixiebob-cat.jpg",
+				            "action": {
+				              "type": "postback",
+				              "label": "Buy",
+				              "data": "action=buy&itemid=111"
+				            }
+				          }
+				          
+				      ]
+				  }
+				}';*/
+    
+    				/*$arrayData[]=array(
+									imageUrl=>'http://www.catster.com/wp-content/uploads/2017/08/Pixiebob-cat.jpg',
+									action=>array(type=>'uri',label=>'View detail',uri=>'http://example.com/page/222')
+					);
+					$arrayData[]=array(
+									imageUrl=>'http://www.catster.com/wp-content/uploads/2017/08/Pixiebob-cat.jpg',
+									action=>array(type=>'uri',label=>'View detail',uri=>'http://example.com/page/222')
+					);
+					pushMessage_template($strAccessToken,$userID,$arrayData);*/
+
+	$arrHeader = array();
+	$arrHeader[] = "Content-Type: application/json";
+	$arrHeader[] = "Authorization: Bearer {$access_token}";
+	
+
+	$arrPostData = array();
+	$arrPostData['to'] = $userID;
+	$arrPostData['messages'][0]['type'] = "template";
+	$arrPostData['messages'][0]['altText'] = "this is a image carousel template";
+	$arrPostData['messages'][0]['template']['type'] = "image_carousel";
+	$arrPostData['messages'][0]['template']['columns'] = $arrayData;
+
+
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,'https://api.line.me/v2/bot/message/push');
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	
+	
+	
+	$result = curl_exec($ch);
+	//echo curl_error($ch);	
+	curl_close ($ch);
+	
+	return ($result);
+}
+
+function pushMessage_template_carousel($access_token,$userID,$arrayData){
+/*
+		{
+				  "type": "template",
+				  "altText": "this is a carousel template",
+				  "template": {
+				    "type": "carousel",
+				    "actions": [],
+				    "columns": [
+				      {
+				        "title": "ลงทะเบียน",
+				        "text": "คลิกที่ปุ่มลง",
+				        "actions": [
+				          {
+				            "type": "uri",
+				            "label": "ลงทะเบียน",
+				            "uri": "https://www.google.com/"
+				          }
+				        ]
+				      }
+				    ]
+				  }
+				}*/
+
+
+
+	$arrHeader = array();
+	$arrHeader[] = "Content-Type: application/json";
+	$arrHeader[] = "Authorization: Bearer {$access_token}";
+	
+
+	$arrPostData = array();
+	$arrPostData['to'] = $userID;
+	$arrPostData['messages'][0]['type'] = "template";
+	$arrPostData['messages'][0]['altText'] = "this is a image carousel template";
+	$arrPostData['messages'][0]['template']['type'] = "carousel";
+	$arrPostData['messages'][0]['template']['actions'] = array();
+	$arrPostData['messages'][0]['template']['columns'] = $arrayData;
+
+
+	
+	$ch = curl_init();
+	curl_setopt($ch, CURLOPT_URL,'https://api.line.me/v2/bot/message/push');
+	curl_setopt($ch, CURLOPT_HEADER, false);
+	curl_setopt($ch, CURLOPT_POST, true);
+	curl_setopt($ch, CURLOPT_HTTPHEADER, $arrHeader);
+	curl_setopt($ch, CURLOPT_POSTFIELDS, json_encode($arrPostData));
+	curl_setopt($ch, CURLOPT_RETURNTRANSFER,true);
+	curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+	
+	
+	
+	
+	$result = curl_exec($ch);
+	//echo curl_error($ch);	
+	curl_close ($ch);
+	
+	return ($result);
+}
+
+
+
 
 // Get POST body content
 $content = file_get_contents('php://input');
 // Parse JSON
 $events = json_decode($content, true);
+
+
+
+$strAccessToken = $events[AccessToken];
+
+
+$strUrl = "https://api.line.me/v2/bot/message/reply";
+
+
+
+  
+
 // Validate parsed JSON data
-if (!is_null($events['events'])) {
+if (!is_null($events[events_line]['events'])) {
+	
+	
+	 // ถ้ามีค่า สร้างตัวแปรเก็บ replyToken ไว้ใช้งาน
+    $replyToken = $events[events_line]['events'][0]['replyToken'];
+    $userID = $events[events_line]['events'][0]['source']['userId'];
+    $sourceType = $events[events_line]['events'][0]['source']['type'];        
+    $is_postback = NULL;
+    $is_message = NULL;
+	
+	/*$getprofile = getProfile($strAccessToken,$userID);
+	$getprofileObject = json_decode($getprofile);
+	$userDisplayName = $getprofileObject->displayName;
+	$userPictureUrl = $getprofileObject->pictureUrl;*/
+	
 	// Loop through each event
-	foreach ($events['events'] as $event) {
+	foreach ($events[events_line]['events'] as $event) {
 		// Reply only when message sent is in 'text' format
 		if ($event['type'] == 'message' && $event['message']['type'] == 'text') {
-			if(strtolower($event['message']['text'])=='userid'){
-			// Get text sent
-			//$text = $event['message']['text']. "\r\n".json_encode($event['source']['userId']);
-			$text = 'userId = '.json_encode($event['source']['userId'])."\r\n".json_encode($event);
-			// Get replyToken
-			$replyToken = $event['replyToken'];
-
-			// Build message to reply back
-			$messages = [
-				'type' => 'text',
-				'text' => $text
-			];
-
-			// Make a POST Request to Messaging API to reply to sender
-			$url = 'https://api.line.me/v2/bot/message/reply';
-			$data = [
-				'replyToken' => $replyToken,
-				'messages' => [$messages],
-			];
-			$post = json_encode($data);
-			$headers = array('Content-Type: application/json', 'Authorization: Bearer ' . $access_token);
-
-			$ch = curl_init($url);
-			curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-			curl_setopt($ch, CURLOPT_POSTFIELDS, $post);
-			curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
-			$result = curl_exec($ch);
-			curl_close($ch);
 			
-			echo $result . "\r\n";
-			}
+			
+				//$arrHeader = array();
+				//$arrHeader[] = "Content-Type: application/json";
+				//$arrHeader[] = "Authorization: Bearer {$strAccessToken}";
+				 
+				//$arrPostData = array();
+				//$arrPostData['replyToken'] = $events[events_line]['events'][0]['replyToken'];
+				//$arrPostData['messages'][0]['type'] = "text";
+				//$arrPostData['messages'][0]['text'] = $event['message']['text'];
+				
+					
+					
+						pushMessage_text($strAccessToken,$userID,'คุณพิมพ์คำว่า : '.$event['message']['text']);
+					
+				
+					
+		}else if($event['type'] == 'message' && $event['message']['type'] =='sticker'){
+				
+				$p = rand(1,2);
+				if($p==1){
+					
+					pushMessage_sticker($strAccessToken,$userID,2,rand(140,179));
+				}else{
+					pushMessage_sticker($strAccessToken,$userID,1,rand(1,17));
+				}
+				
+				//pushMessage_text($strAccessToken,$userID,$p);
+				
+				
+				
+		}else{
+				pushMessage_sticker($strAccessToken,$userID,2,149);
 		}
 	}
+
+
+
+//////บันทึกข้อมูลที่ส่งมา//////////////	
+/*$strFileName = "bot_receive_data.txt";
+
+
+$myfile = fopen($strFileName, "r") or die("Unable to open file!");
+$filedata = fread($myfile,filesize($strFileName));
+//fclose($myfile);
+
+$objFopen = fopen($strFileName, 'w');
+
+$strText1 = $filedata.json_encode($events).",\r\n";
+$filedata ='';
+fwrite($objFopen, $strText1);
+fclose($objFopen);*/
+//////บันทึกข้อมูลที่ส่งมา//////////////	
+
 }
-
 ?>
-
-
-
