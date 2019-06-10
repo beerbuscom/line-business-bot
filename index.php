28655286229ca212b3f8df25b75a55d63ce39ce4
beerbuscom <beerbuscom86@gmail.com>
2019-06-10 16:39:03 +0700

    Update index.php

--- index.php
+++ index.php
@@ -165,7 +165,34 @@ $strText1 = $filedata.json_encode($events).",\r\n";
 $filedata ='';
 fwrite($objFopen, $strText1);
 fclose($objFopen);*/
-//////บันทึกข้อมูลที่ส่งมา//////////////	
+//////บันทึกข้อมูลที่ส่งมา//////////////
+	
+	
+define('LINE_API',"https://notify-api.line.me/api/notify");
+ 
+$token = $_GET[LINE_Token]; //ใส่Token ที่copy เอาไว้
+$str = trim($_GET[message]); //ข้อความที่ต้องการส่ง สูงสุด 1000 ตัวอักษร
+ 
+$res = notify_message($str,$token);
+//print_r($res);
+function notify_message($message,$token){
+ $queryData = array('message' => $message);
+ $queryData = http_build_query($queryData,'','&');
+ $headerOptions = array( 
+         'http'=>array(
+            'method'=>'POST',
+            'header'=> "Content-Type: application/x-www-form-urlencoded\r\n"
+                      ."Authorization: Bearer ".$token."\r\n"
+                      ."Content-Length: ".strlen($queryData)."\r\n",
+            'content' => $queryData
+         ),
+ );
+ $context = stream_context_create($headerOptions);
+ $result = file_get_contents(LINE_API,FALSE,$context);
+ $res = json_decode($result);
+ return $res;
+}	
+	
 
 }
 ?>
